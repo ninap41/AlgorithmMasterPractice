3c051f8b50c34bf688c63043b0b1d60a5675a24b
ninap41 <ninapalumbo@gmail.com>
2018-01-23 10:35:36 -0600

    Added basic practice

---
+++ algoMaster.js
@@ -0,0 +1,45 @@
+
+// ****THIS DOCUMENT WILL BE A MASTER OF ALL MY ALGORITHM
+// ***** WORK PREPPING FOR A JOB HUNT, STARTING WITH MOST 
+// ***** BASIC ALL THE WAY TO DATA STRUCTURES AND BIT 
+// ***** MANIPULATION. *******
+
+
+// print sum between 1-255 ///////////////////////////////////////////////////////////////
+sum= 0;
+function printSum255_withDivisibleByFive(){
+    for(var i= 9; i <=255; i++){
+        sum += i;
+        if(i % 5 == 0){
+            console.log(i + " is divisible by 5")
+        }
+        else{
+        console.log(i)
+        }
+    }
+    console.log(`${sum} Is the Sum!`)
+}
+// printSum255_withDivisibleByFive()
+
+// FizzBuzz ///////////////////////////////////////////////////////////////
+
+sum= 0;
+function FizzBuzz(){
+    for(var i= 9; i <=100; i++){
+       
+        if(i % 3 == 0){
+            console.log("Fizz")
+        }
+        else if (i % 5 ==0){
+            console.log("Buzz")
+        }
+        else{
+            console.log(i + " is not a Buzz or a Fizz!")
+        }
+    } 
+}
+
+// FizzBuzz()
+
+// FizzBuzz ///////////////////////////////////////////////////////////////
+
